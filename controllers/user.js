9869a96621bfc32512496b82bac60239828db99b
hieusStorm <hyrum555@gmail.com>
2025-03-22 08:12:32 -0700

    added user routes

--- controllers/user.js
+++ controllers/user.js
@@ -3,19 +3,30 @@ const ObjectId = require("mongodb").ObjectId;
 
 // functions
 const getAll = async (req, res) => {
-    const result = await mongodb.getDB().db().collection('user').find();
-    result.toArray().then((user) => {
+    //#swagger.tags['Monsters']
+    const result = await mongodb.getDB().db().collection('monster').find();
+    result.toArray((err, monsters) => {
+        if(err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(user);
+        res.status(200).json(monsters);
     });
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDB().db().collection("user").find({_id: userId});
-    result.toArray().then((user) => {
+    //#swagger.tags['Monsters']
+    if(!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid monster ID to find.");
+    }
+    const monsterId = new ObjectId(req.params.id);
+    const result = await mongodb.getDB().db().collection("monster").find({_id: monsterId});
+    result.toArray((err, monster) => {
+        if(err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(user[0]);
+        res.status(200).json(monster[0]);
     });
 };
 
@@ -33,8 +44,46 @@ const createUser = async (req, res) => {
     }
 }
 
+const updateUser = async (req, res) => {
+    //#swagger.tags['Users']
+    if(!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid user ID to update.");
+    }
+    const userId = new ObjectId(req.params.id);
+
+    const user = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    const response = await mongodb.getDB().db().collection("user").replaceOne({_id: userId}, user);
+
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "An error occured while updating the user");
+    }
+};
+
+const deleteUser = async (req, res) => {
+    //#swagger.tags['Users']
+    if(!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid user ID to delete.");
+    }
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDB().db().collection("user").deleteOne({_id: userId});
+    
+    if(response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || `An error occured while deleting the user ${userId}`);
+    }
+};
+
 module.exports = {
     getAll,
     getSingle,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const controller = require("../controllers/user");
+const validate = require('../middleware/validate');
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getSingle);
-router.post("/", controller.createUser);
+router.post("/", validate.saveUser, controller.createUser);
+router.put("/:id", validate.saveUser, controller.updateUser);
+router.delete("/:id", controller.deleteUser);
 
 module.exports = router;

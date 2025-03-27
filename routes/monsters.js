69120a192553f97d84ce17c2fdabb10c2948330d
hieusStorm <hyrum555@gmail.com>
2025-03-27 11:14:26 -0700

    Added Oauth

--- routes/monsters.js
+++ routes/monsters.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const controller = require("../controllers/monster");
+const { isAuthenticated } = require("../middleware/authenticate");
 const validate = require('../middleware/validate');
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getSingle);
-router.post("/", validate.saveMonster, controller.createMonster);
-router.put("/:id", validate.saveMonster, controller.updateMonster);
-router.delete("/:id", controller.deleteMonster);
+router.post("/", isAuthenticated, validate.saveMonster, controller.createMonster);
+router.put("/:id", isAuthenticated, validate.saveMonster, controller.updateMonster);
+router.delete("/:id", isAuthenticated, controller.deleteMonster);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const controller = require("../controllers/user");
+const { isAuthenticated } = require("../middleware/authenticate");
 const validate = require('../middleware/validate');
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getSingle);
-router.post("/", validate.saveUser, controller.createUser);
-router.put("/:id", validate.saveUser, controller.updateUser);
-router.delete("/:id", controller.deleteUser);
+router.post("/", isAuthenticated, validate.saveUser, controller.createUser);
+router.put("/:id", isAuthenticated, validate.saveUser, controller.updateUser);
+router.delete("/:id", isAuthenticated, controller.deleteUser);
 
 module.exports = router;
